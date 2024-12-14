c972177445bd4b4bdbd1608c114b440d0db8243b
Carl <a13136053@gmail.com>
2024-12-14 22:13:34 +0500

    UPDATED

--- Main/ui/uwuware.lua
+++ Main/ui/uwuware.lua
@@ -3405,7 +3405,7 @@ do
 		local function getAllConfigs()
 			local files = {};
 
-			for _, v in next, listfiles('QQ_6699-projects/configs') do
+			for _, v in next, listfiles('vocats-projects/configs') do
 				if (not isfolder(v)) then continue; end;
 
 				for _, v2 in next, listfiles(v) do
